173887c0c9427bf43e7ca264181deabf9f6030ab
Alima <57910466+aaglakova@users.noreply.github.com>
2019-11-20 19:10:19 +0600

    Create task4.cpp

---
+++ task4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char a='A';
+    for (int i=0;i<5;i++)
+    {
+        for(int j=0;j<8;j++)
+        cout<<a;
+        cout<<endl;
+    }
+    return 0;
+}
