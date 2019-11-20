dd6214e972dcdb3d5af8be2f7cfd9776ed652b0b
Alima <57910466+aaglakova@users.noreply.github.com>
2019-11-20 21:57:06 +0600

    Create task21.cpp

---
+++ task21.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    double a,b,c;
+    cin>>a>>b;
+    cout<<(a*b)/2<<endl;
+    cout<<a+b+c<<endl;
+    c=sqrt(a*a+b*b);
+    cout<<c;    
+}
