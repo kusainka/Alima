96331b87aaf63ab526568a7e4b8658d08fed8ad1
Alima <57910466+aaglakova@users.noreply.github.com>
2019-11-20 19:02:24 +0600

    Create task3.cpp

---
+++ task3.cpp
@@ -0,0 +1,15 @@
+//task 3.
+#include <iostream>
+using namespace std;
+int main(){
+    int a=0;
+    int count=1;
+    for (int i=0;i<5;i++)
+    {
+        for(int j=0;j<count;j++)
+        cout<<a ;
+        cout<<endl ;
+        count++;
+    }
+    return 0;
+}
